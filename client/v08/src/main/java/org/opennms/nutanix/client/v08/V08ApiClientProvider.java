83c271759f3b900097cd6093ede541c5cb7d7126
Antonio <rssntn67@yahoo.it>
2023-08-12 09:06:38 +0200

    Fixed an error

--- client/v08/src/main/java/org/opennms/nutanix/client/v08/V08ApiClientProvider.java
+++ client/v08/src/main/java/org/opennms/nutanix/client/v08/V08ApiClientProvider.java
@@ -8,13 +8,14 @@ import javax.ws.rs.core.HttpHeaders;
 import org.opennms.nutanix.client.api.ApiClient;
 import org.opennms.nutanix.client.api.ApiClientCredentials;
 import org.opennms.nutanix.client.api.ApiClientProvider;
+import org.opennms.nutanix.client.api.V08ClientProvider;
 import org.opennms.nutanix.client.api.model.ApiVersion;
 import org.opennms.nutanix.client.v08.api.HaApi;
 import org.opennms.nutanix.client.v08.handler.ApiException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class V08ApiClientProvider implements ApiClientProvider {
+public class V08ApiClientProvider implements V08ClientProvider {
     private static final Logger LOG = LoggerFactory.getLogger(V08ApiClientProvider.class);
 
     public V08ApiClientProvider() {
