a2d8c30d58b979cec01f50b73d84cb554de9e1dd
Antonio Russo <antonio@MacBook-Pro-di-Antonio.local>
2024-09-27 19:49:08 +0200

    Fixed setIgnoreSslValidation

--- client/v3/src/main/java/org/opennms/nutanix/client/v3/ApiClientExtension.java
+++ client/v3/src/main/java/org/opennms/nutanix/client/v3/ApiClientExtension.java
@@ -1,9 +1,59 @@
 package org.opennms.nutanix.client.v3;
 
+import okhttp3.OkHttpClient;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
 public class ApiClientExtension extends org.opennms.nutanix.client.v3.handler.ApiClient {
 
     int pageSize = 30;
-    boolean ignoreSslCertificateValidation = false;
+
+    /*
+     * This is very bad practice and should NOT be used in production.
+     */
+    private static final TrustManager[] trustAllCerts = new TrustManager[] {
+            new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                }
+
+                @Override
+                public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                }
+
+                @Override
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return new java.security.cert.X509Certificate[]{};
+                }
+            }
+    };
+    private static final SSLContext trustAllSslContext;
+    static {
+        try {
+            trustAllSslContext = SSLContext.getInstance("SSL");
+            trustAllSslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static final SSLSocketFactory trustAllSslSocketFactory = trustAllSslContext.getSocketFactory();
+
+    /*
+     * This should not be used in production unless you really don't care
+     * about the security. Use at your own risk.
+     */
+    public static OkHttpClient trustAllSslClient(OkHttpClient client) {
+        OkHttpClient.Builder builder = client.newBuilder();
+        builder.sslSocketFactory(trustAllSslSocketFactory, (X509TrustManager)trustAllCerts[0]);
+        builder.hostnameVerifier((hostname, session) -> true);
+        return builder.build();
+    }
+
     public Integer getLength() {
         return pageSize;
     }
@@ -12,7 +62,8 @@ public class ApiClientExtension extends org.opennms.nutanix.client.v3.handler.Ap
         this.pageSize = length;
     }
 
-    public void setIgnoreSslCertificateValidation(boolean b) {
-        this.ignoreSslCertificateValidation = b;
+    public void setIgnoreSslCertificateValidation(boolean ignoreSslCertificateValidation) {
+        if (ignoreSslCertificateValidation)
+            setHttpClient(trustAllSslClient(getHttpClient()));
     }
 }
